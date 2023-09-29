0581773a7f758260a7d371bdb42204e86d902541
andreabrugnoli <a.brugnoli@utwente.nl>
2023-09-29 22:22:40 +0200

    changing degree does not change

--- tests/cantilever_beam/run_cantilever.py
+++ tests/cantilever_beam/run_cantilever.py
@@ -1,12 +1,12 @@
-from cantilever_beam import simulate_cantilever_beam
-# from cantilever_beam_nedelec import simulate_cantilever_beam
+# from cantilever_beam import simulate_cantilever_beam
+from cantilever_beam_nedelec import simulate_cantilever_beam
 import src.postprocessing.options
 import matplotlib.pyplot as plt
 import numpy as np
 
 isquad = False
 time_vector, energy_vector_nonlinear, power_balance_vector_nonlinear = \
-    simulate_cantilever_beam(is_quad_mesh=isquad, linear=False, pol_degree=1, n_elem_x=200)
+    simulate_cantilever_beam(is_quad_mesh=isquad, linear=False, pol_degree=1, n_elem_x=100)
 
 # time_vector, energy_vector_linear, power_balance_vector_linear = \
 #     simulate_cantilever_beam(is_quad_mesh=isquad, linear=True)
