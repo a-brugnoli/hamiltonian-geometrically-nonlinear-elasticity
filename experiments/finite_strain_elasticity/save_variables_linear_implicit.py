a632a1028d9453d2605ceac50cb7c46dbb2fd718
andreabrugnoli <a.brugnoli5@gmail.com>
2025-05-25 03:10:51 +0200

    Stress in bending column

--- experiments/finite_strain_elasticity/save_variables_linear_implicit.py
+++ experiments/finite_strain_elasticity/save_variables_linear_implicit.py
@@ -3,6 +3,6 @@ import pickle
 import numpy as np
 from experiments.finite_strain_elasticity.parameters_elasticity import *
 
-bending_column.set_time_step(dt_reference)
-dict_results_reference = bending_column.linear_implicit(save_vars=False,\
+bending_column.set_time_step(dt_base)
+dict_results_reference = bending_column.linear_implicit_static_condensation(save_vars=False,\
                                                         paraview_directory=paraview_directory)
